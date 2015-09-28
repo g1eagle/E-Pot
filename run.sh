b43501294544850709e000603e8788079a71f8e8
Steven Whitelock <g1eagle@gmail.com>
2015-09-28 02:34:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,27 @@
+#install docker (skip if you have docker 1.3+ already)
+[ -e /usr/lib/apt/methods/https ] || {
+  sudo apt-get update
+  sudo apt-get install apt-transport-https
+}
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys \
+    36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > \
+    /etc/apt/sources.list.d/docker.list"
+
+sudo apt-get update
+sudo apt-get -y install lxc-docker
+
+#install docker-compose
+sudo apt-get install -y python-pip
+sudo pip install docker-compose
+
 #/usr/bin/env bash
 DIR=$(cd $(dirname "$0"); pwd)
 KIPPO_UID=499:499
 DIONAEA_UID=nobody:nogroup
-YML=https://raw.githubusercontent.com/g1eagle/E-Pot/master/docker-compose.yml
+YML=https://raw.githubusercontent.com/andrewmichaelsmith/manuka/master/docker-compose.yml
 
 #donwload/update yml
 curl -q $YML > docker-compose.yml
