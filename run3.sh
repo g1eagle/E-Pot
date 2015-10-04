23692d2e55e087b287ff6816bcddb844c70c50fe
Steven Whitelock <g1eagle@gmail.com>
2015-10-03 21:09:42 -0500

    fix user input

--- run3.sh
+++ run3.sh
@@ -7,13 +7,14 @@
 echo "Install docker? [y/n]"
 read docker
 
-if ["$docker" == "y"]
+if ["$docker" == "y"]; then
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
 	sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
 	sudo apt-get update
 	sudo apt-get -y install lxc-docker
+	exit 1
 
 #install docker-compose
 #sudo apt-get install -y python-pip
