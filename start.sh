b0918974eebef3afeec9a0a4ddef67e29633f9ba
Steven Whitelock <g1eagle@users.noreply.github.com>
2015-10-16 02:29:49 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,9 +13,27 @@ while [ "$RUNNING"  == false ]; do
     RUNNING=$(sudo docker inspect --format '{{.Sate.Running}}' mysql)
 done
 
-MYSQLIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mysql)
-cp $DIR/var/glastopf/glastopf.cfg2 $DIR/var/glastopf/glastopf.cfg
-sed -i "s/-localgost-/$MYSQLIP/" $DIR/var/glastopf.cfg
+RUNNING=$(sudo docker inspect --format '{{.State.Running}}' dionaea)
+while [ "$RUNNING"  == false ]; do
+    if [ "$RUNNING" == false ]
+    then
+       # do something wise
+        sudo docker start dionaea
+        echo "running dionaea"
+    fi
+    sleep 20s
+    RUNNING=$(sudo docker inspect --format '{{.State.Running}}' dionaea)
+done
+
+
+sleep 30s
+MYSQLIP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' mysql)
+echo "$MYSQLIP"
+sudo cp $DIR/var/glastopf/glastopf.cfg2 $DIR/var/glastopf/glastopf.cfg
+sudo cp $DIR//var/kippo/kippo.cfg2 $DIR/var/kippo/kippo.cfg
+sudo sed -i "s/-localhost-/$MYSQLIP/" $DIR/var/glastopf/glastopf.cfg
+sudo sed -i "s/-localhost-/$MYSQLIP/" $DIR/var/kippo/kippo.cfg
+
 
 RUNNING=$(sudo docker inspect --format '{{.State.Running}}' glastopf)
 while [ "$RUNNING"  == false ]; do
@@ -40,16 +58,4 @@ while [ "$RUNNING"  == false ]; do
     fi
     sleep 20s
     RUNNING=$(sudo docker inspect --format '{{.State.Running}}' kippo)
-done
-
-RUNNING=$(sudo docker inspect --format '{{.State.Running}}' dionaea)
-while [ "$RUNNING"  == false ]; do
-    if [ "$RUNNING" == false ]
-    then
-       # do something wise
-        sudo docker start dionaea
-        echo "running dionaea"
-    fi
-    sleep 20s
-    RUNNING=$(sudo docker inspect --format '{{.State.Running}}' dionaea)
 done
